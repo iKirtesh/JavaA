37a15a48aacd21c328f393c20c6d4a8d9a6779dd
Kirtesh  Admute <59656771+iKirtesh@users.noreply.github.com>
2023-01-05 02:31:38 +0530

    Update Shape.java

--- Assignment 4/Shape.java
+++ Assignment 4/Shape.java
@@ -49,3 +49,18 @@ public class Rectangle extends Shape {
     }
 
 }
+
+
+public class Triangle extends Shape {
+
+    double area;
+
+    public double compute_area() {
+        height = this.getHeight();
+        width = this.getWidth();
+
+        area = (height * width) / 2;
+        return area;
+    }
+
+}
