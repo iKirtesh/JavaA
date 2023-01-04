e217d4aa6a88589ff4ca03c2b3782074fda54c88
Kirtesh  Admute <59656771+iKirtesh@users.noreply.github.com>
2023-01-05 02:28:25 +0530

    Update Publication.java

--- Assignment 2/Publication.java
+++ Assignment 2/Publication.java
@@ -100,3 +100,19 @@ public class Magazine extends Publication {
     }
 
 }
+
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        Publication obj = new Publication("Java", 200, 5);
+        obj.sellcopy(2);
+
+        Publication obj1 = new Book("Java", 100, 10, "James Gosling");
+        obj1.sellcopy(3);
+
+        Publication obj2 = new Magazine("Java", 200, 5, 6, "September");
+        obj2.sellcopy(4);
+
+    }
+}
