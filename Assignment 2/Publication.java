af55658d87f3c91af320ad6fa370bca92be3e183
Kirtesh  Admute <59656771+iKirtesh@users.noreply.github.com>
2023-01-05 02:27:21 +0530

    Update Publication.java

--- Assignment 2/Publication.java
+++ Assignment 2/Publication.java
@@ -38,3 +38,37 @@ public class Publication {
     }
 
 }
+
+
+
+
+
+public class Book extends Publication {
+
+    private String author;
+
+    // Define constructor to assign data members
+
+    public Book() {
+        super();
+        this.author = null;
+
+    }
+
+    public Book(String title, double price, int copies, String author) {
+        super(title, price, copies);
+        this.author = author;
+    }
+
+    public void ordercopies(int pcopies) {
+        setcopies(getcopies() + pcopies);
+
+    }
+
+    // Method sellcopy() of superclass publication is overridden
+
+    public void sellcopy(int qty) {
+        System.out.println("Total book sell : Rs. " + (qty * getprice()));
+    }
+
+}
